490aa54572c33ccde94191e47ed745d43db899d5
mildenvvv <ivan.charniausk@infotech.edu.pl>
2025-06-05 13:40:19 +0200

    web

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
+import os
 def load_config():
     return {
         "bot_token": '7765773913:AAGYSaR0oInpRFZ0ZF4bVUGwVO6s43ORUtY',
-        "DATABASE_URL": 'sqlite:///user_data.db'
+        "DATABASE_URL": 'sqlite:///user_data.db',
+        "webhook_url": os.environ.get("WEBHOOK_URL", "https://example.com")
     }
